01f61e8f9a9f4acbb56cc0f597b7d2ef1278ecbe
Prashrijan <shresthaprashrijan@gmail.com>
2025-02-17 13:49:58 +1100

    boiler plate code

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT;
+
+app.get("/api/healthcheck", (req, res) => {
+    res.send("Server is running");
+});
+
+app.listen(PORT, () => {
+    try {
+        console.log(
+            `Server is ready and listening at port http://localhost:${PORT}`
+        );
+    } catch (error) {
+        console.log(`Error connecting to the server: ${error}`);
+    }
+});
